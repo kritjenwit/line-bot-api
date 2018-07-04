dd9de27efd18faaba350c046e41b8c501602f596
Jenwit_Krit <jenwitkmonz@gmail.com>
2018-07-04 15:00:29 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 
 // example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-01.php
 
-include ('line-bot-api/php/line-bot.php');
+include ('php/line-bot.php');
 
 $channelSecret = '9888fe460c0829ae8ecb181c6cf9ad3a';
 $access_token  = 'vTvmYMZEHXc+NctGN3n1ZmBffzkO3sROq1HyP3Oegbzjqr92zV1vD+Gw8WBPXFttjGIM4Zp0EIkybpSbl1z6Dwri54Qgtrq4qaRte+x/j23ww5wbICrayMunudQbaAy9v4eMw+cEGfhBHAmKz7h28wdB04t89/1O/w1cDnyilFU=';
